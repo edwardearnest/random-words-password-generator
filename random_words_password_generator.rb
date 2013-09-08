003f3faa16ccceb544bec2612083fd3d3964402a
Edward Earnest <edwardearnest2015@gmail.com>
2013-09-07 19:18:05 -0500

    Filtering of word list, install instructions, remove trailing period and newlines from final password.

--- random_words_password_generator.rb
+++ random_words_password_generator.rb
@@ -1,14 +1,35 @@
+#!/usr/bin/env ruby
 # random_words_password_generator.rb
 #
 # Created: 2013-09-05
-# Revised: 2013-09-05
+# Revised: 2013-09-07
 #
 # Copyright 2013 by Edward Earnest. 
 # Licensed under GNU GPLv3. Please see file LICENSE.
-# 
-# Gives you a password consisting of five random period-delimited
-# words in the form: 
+#
+# SUMMARY: 
+#
+#  This program gives you a password consisting of five random
+#  period-delimited words in the form: 
+#
 #      word1.word2.word3.word4.word5
+#
+# GETTING STARTED & SYSTEM REQUIREMENTS:
+# 
+#  Any *nix-like system with a wordlist living in /usr/share/dict/words
+#  Windows users may substitue a wordlist of their choice.
+#  Open an issue on GitHub if you need help with this. 
+#
+#  1. Install Ruby (https://www.ruby-lang.org/) & ensure a word list is present.
+#         Debian/Ubuntu: 
+#         $ sudo apt-get install ruby wordlist
+#  
+#     Written for Ruby 2.0, but should work for 1.9 branch and maybe 1.8 branch.
+# 
+#  2. Run this file.
+#         $ ruby random_words_password_generator.rb
+# 
+# NOTES:
 #
 # I call this the XKCD-method [1], which should [2] be cryptographically
 # secure so long as:
@@ -21,72 +42,97 @@
 #       Note: SecureRandom appears to fall back to /dev/urandom on
 #       *nix-type systems if OpenSSL isn't available; it might be
 #       worth looking into a /dev/random solution, but for the moment
-#       this is Good Enough for me.
+#       this is Good Enough for me & I'll trust that OpenSSL and the
+#       Ruby Core Team know more about this than I do.
 #
 #  3. Because these are words and "not very entropy-dense", you
 #     need to be careful about leaking enough of your password that
-#     someone could guess the rest.  You don't want someone to see
-#     you typing it in and deduce that you are typing in
-#       fiv?.w???s.as.y??r.pa??word 
+#     someone could guess the rest.  For example, you don't want 
+#     someone to see you typing it in and deduce that you are typing:
 #
-# Acknowledgments:
-#   Randall Munroe, for the original comic and the insight that
-#       reasonable human-memorizable passwords are stronger than more
-#       computer-oriented passwords that you keep on a sticky note on
-#       your computer monitor.
-#                                        cryptographic
+#         fiv?.w???s.as.y??r.pa??word 
+#
+# ACKNOWLEDGMENTS:
+#
+#   Randall Munroe of xkcd.com, for the original comic and the insight
+#       that reasonable human-memorizable passwords are stronger than 
+#       more computer-oriented passwords that you keep on a sticky note 
+#       on your computer monitor.
+#
 #   Colin Percival, for his input on the cryptographic integrity of
-#       this method. If you like this, check out Tarsnap [3] - secure online
-#       backups.
+#       this method. If you like this, check out Tarsnap [3] - secure
+#       online backups.
 #
 # While both the pattern of the overall string & the fact that we are
 # using words are both known, so long as the words are truly random
 # and not chosen because they hold meaning for the end user, we should
-# be fine.
-#
+# be fine. 
+# 
 # TL;DR - let the machine pick & accept what it picks. 
 #
-# Bonus - your vocabulary improves!
+# REFERENCES:
+# 
+#   [1] https://xkcd.com/936/
+#   [2] Disclaimer: I am not cryptography or information theory
+#         expert. This is a hobby program I created on my own time that
+#         should be sufficient to increase my own password security
+#         enough for the moment.  I would sleep better if this used
+#         /dev/random, but in a choice between more research into the
+#         random mechanics of OpenSSL vs. /dev/(u)random and getting
+#         this finished and getting sleep, I choose the latter.
+#   [3] Tarsnap is secure backups for Unix geeks:
+#           <https://www.tarsnap.com/>
+#           "Online backups for the truly paranoid." 
+#       Personally, I use cron & Tarsnap-generations to manage my 
+#       Tarsnap backups: <https://github.com/Gestas/Tarsnap-generations>
 #
-# [1] https://xkcd.com/936/
-# [2] Disclaimer: I am not cryptography or information theory
-#       expert. This is a hobby program I created on my own time that
-#       should be sufficient to increase my own password security
-#       enough for the moment.  I would sleep better if this used
-#       /dev/random, but in a choice between more research into the
-#       random mechanics of OpenSSL vs. /dev/(u)random and getting
-#       this finished and getting sleep, I choose the latter.
-# [3] Tarsnap is secure backups for Unix geeks:
-#      <https://www.tarsnap.com/>
-#       "Online backups for the truly paranoid." 
-#     I use cron & Tarsnap-generations to manage my Tarsnap backups.
-#      <https://github.com/Gestas/Tarsnap-generations>
-#
+###
+# TODO
+# I've tested these, but having unit tests to prove them would be great:
+# -- filtering of apostrophies
+# -- filtering of anything capitalized
+# -- newline removal
+# -- final password is of the form <word>.<word>.<word>.<word> 
+# -- final password, as above, has no trailing dot
+###
 require 'securerandom'
-array_of_dictionary_words = IO.readlines('/usr/share/dict/words')
-max_words = array_of_dictionary_words.length
 
-# TODO - filter out words with apostorphie's.
-# TODO MAYBE - filter out Proper Names? (anything with initial uppercase?)
+puts "Reading wordlist..."
+list_of_words = IO.readlines('/usr/share/dict/words')
 
-"We are working with #{max_words} words!"
+# Remove any proper nouns (anything with initial capitalization) and
+# also any words with apostrophies.
+puts "Processing wordlist..."
+list_of_words = list_of_words.delete_if {|word| word.include?("\'")}
+list_of_words = list_of_words.delete_if {|word| word[0].match("[A-Z]")}
+
+# Remove newlines
+list_of_words = list_of_words.map { |word| word.chomp }
+max_words = list_of_words.length
+
+puts "We are working with #{max_words} words!"
+puts ""
 
 # While arrays are zero-indexed in Ruby, we don't need to adjust for
 # this when determining max_words because SecureRandom.random_number
 # will return a number < max_words
 password = ''
-5.times do |number|
-  puts "Generating word number #{number + 1}..."
+puts "Generating password (this could take a little while)..."
+
+5.times do
   array_index = SecureRandom.random_number(max_words)
-  word = array_of_dictionary_words[array_index]
-  password << word << '.' # TODO Fix so all on one line; or is that a
-                          # feature not a bug?
+  word = list_of_words[array_index]
+  password << word << '.'
 end
 
+# Remove the final period.
+password.chomp!(".") 
+
 # TODO - citation needed - will changing the order reduce entropy?
+puts ""
 puts "Your new password has been generated; if you don't like it, you
-may change the order, but that will reduce the ethropy slightly.
-[citation needed]" 
+may change the order, but that will reduce the ethropy slightly."
 puts "" # formatting
-puts "Your password is: #{password}"
+puts "   Your new password is: #{password}"
+puts "" # formatting
 puts "Keep it safe." 
